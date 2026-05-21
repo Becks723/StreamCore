58b3370d2f2f4b24130479c9f1809351225380f4
Becks723 <1991737469@qq.com>
2026-05-21 19:35:09 +0800

    fix: like db atomic

--- internal/pkg/db/interaction/create_like.go
+++ internal/pkg/db/interaction/create_like.go
@@ -6,22 +6,28 @@ import (
 
 	"StreamCore/internal/pkg/constants"
 	"StreamCore/internal/pkg/db/model"
+	"gorm.io/gorm"
 )
 
 func (repo *iactiondb) CreateLike(ctx context.Context, tarType int, uid, tarId uint, time time.Time) error {
-	// TODO: atomic
-	repo.db.Model(&model.LikeRelationModel{}).Create(&model.LikeRelationModel{
-		Uid:        uid,
-		TargetType: tarType,
-		TargetId:   tarId,
-		Status:     constants.LikeAction_Like,
-		Time:       time,
+	return repo.db.Transaction(func(tx *gorm.DB) error {
+		if err := tx.Model(&model.LikeRelationModel{}).Create(&model.LikeRelationModel{
+			Uid:        uid,
+			TargetType: tarType,
+			TargetId:   tarId,
+			Status:     constants.LikeAction_Like,
+			Time:       time,
+		}).Error; err != nil {
+			return err
+		}
+		if err := tx.Model(&model.LikeCountModel{}).Create(&model.LikeCountModel{
+			TargetType:  tarType,
+			TargetId:    tarId,
+			LikeCount:   1,
+			UnlikeCount: 0,
+		}).Error; err != nil {
+			return err
+		}
+		return nil
 	})
-	repo.db.Model(&model.LikeCountModel{}).Create(&model.LikeCountModel{
-		TargetType:  tarType,
-		TargetId:    tarId,
-		LikeCount:   1,
-		UnlikeCount: 0,
-	})
-	return nil
 }
--- internal/pkg/db/interaction/toggle_like_status.go
+++ internal/pkg/db/interaction/toggle_like_status.go
@@ -24,24 +24,33 @@ func (repo *iactiondb) ToggleLikeStatus(ctx context.Context, tarType int, uid, t
 }
 
 func (repo *iactiondb) switchToLike(ctx context.Context, tarType int, uid, tarId uint) error {
-	// TODO: atomic
-	repo.db.Model(&model.LikeRelationModel{}).
-		Where("target_type = ? AND uid = ? AND target_id = ?", tarType, uid, tarId).
-		Update("status", constants.LikeAction_Like)
-	repo.db.Model(&model.LikeCountModel{}).
-		Where("target_type = ? AND target_id = ?", tarType, tarId).
-		Update("like_count", gorm.Expr("like_count + ?", 1))
-	return nil
+	return repo.db.Transaction(func(tx *gorm.DB) error {
+		if err := tx.Model(&model.LikeRelationModel{}).
+			Where("target_type = ? AND uid = ? AND target_id = ?", tarType, uid, tarId).
+			Update("status", constants.LikeAction_Like).Error; err != nil {
+			return err
+		}
+		if err := tx.Model(&model.LikeCountModel{}).
+			Where("target_type = ? AND target_id = ?", tarType, tarId).
+			Update("like_count", gorm.Expr("like_count + ?", 1)).Error; err != nil {
+			return err
+		}
+		return nil
+	})
 }
 
 func (repo *iactiondb) switchToUnlike(ctx context.Context, tarType int, uid, tarId uint) error {
-	// TODO: atomic
-	// TODO: atomic
-	repo.db.Model(&model.LikeRelationModel{}).
-		Where("target_type = ? AND uid = ? AND target_id = ?", tarType, uid, tarId).
-		Update("status", constants.LikeAction_Unlike)
-	repo.db.Model(&model.LikeCountModel{}).
-		Where("target_type = ? AND target_id = ?", tarType, tarId).
-		Update("unlike_count", gorm.Expr("unlike_count + ?", 1))
-	return nil
+	return repo.db.Transaction(func(tx *gorm.DB) error {
+		if err := tx.Model(&model.LikeRelationModel{}).
+			Where("target_type = ? AND uid = ? AND target_id = ?", tarType, uid, tarId).
+			Update("status", constants.LikeAction_Unlike).Error; err != nil {
+			return err
+		}
+		if err := tx.Model(&model.LikeCountModel{}).
+			Where("target_type = ? AND target_id = ?", tarType, tarId).
+			Update("unlike_count", gorm.Expr("unlike_count + ?", 1)).Error; err != nil {
+			return err
+		}
+		return nil
+	})
 }
